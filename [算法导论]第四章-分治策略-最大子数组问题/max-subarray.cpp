a7a735f6416117fa38ea7aafff098502dc45e389
liao <mail@liaozhibin.com>
2019-08-11 22:33:29 +0800

    blog

--- [算法导论]第四章-分治策略-最大子数组问题/max-subarray.cpp
+++ [算法导论]第四章-分治策略-最大子数组问题/max-subarray.cpp
@@ -1,31 +1,97 @@
 #include <iostream>
-#include <tuple>
 using namespace std;
 
-int P[13] = {23, 13, 15, 32, 25, 27, 19, 20, 16, 12, 22, 24, 18};
+int P[16] = {23,11, 13, 5, 6, 15, 32, 25, 27, 19, 20, 16, 12, 22, 24, 18};
 
 // 变换成差分
 void transformArray(int list[], int len) {
     for (int i = 0; i < len - 1; i++) {
         list[i] = list[i + 1] - list[i];
     }
+    list[len-1] = 0;
 }
 
-tuple<int, int*, int*> findMaxSubArray(int* low, int* high);
-tuple<int, int*, int*> findMaxSubArrayCross(int* low, int* mid, int* high);
+int findMaxSubArray(int *&low, int *&high);
+int findMaxSubArrayCross(int *&low, int *&mid, int *&high);
 
 // 注意左闭右开
-tuple<int, int*, int*> findMaxSubArray(int* low, int* high) {
+int findMaxSubArray(int *&low, int *&high) {
     if (high - low == 1) {
-        return {*low, low, low+1};
-    } else if (high - low == 2) {
-        if (*low > *high) {
-            return {*low, low, low+1};
-        } else {
-            return {*(low+1), low+1, low+2};
-        }
+        return *low;
     } else {
-        int* mid = low + (high - low) / 2;
-        
+        int *mid = low + (high - low) / 2;
+
+        int *leftLow = low;
+        int *leftHigh = mid;
+        int leftMax = findMaxSubArray(leftLow, leftHigh);
+
+        int *rightLow = mid;
+        int *rightHigh = high;
+        int rightMax = findMaxSubArray(rightLow, rightHigh);
+
+        int *midLow = low;
+        int *midHigh = high;
+        int midMax = findMaxSubArrayCross(midLow, mid, midHigh);
+
+        if (leftMax > rightMax) {
+            if (leftMax > midMax) {
+                low = leftLow;
+                high = leftHigh;
+                return leftMax;
+            } else {
+                low = midLow;
+                high = midHigh;
+                return midMax;
+            }
+        } else if (rightMax > midMax) {
+            low = rightLow;
+            high = rightHigh;
+            return rightMax;
+        } else {
+            low = midLow;
+            high = midHigh;
+            return midMax;
+        }
+
     }
+}
+
+int findMaxSubArrayCross(int *&low, int *&mid, int *&high) {
+    int *maxLow = mid - 1;
+    int maxLeft = -9999;
+    int *maxHigh = mid;
+    int maxRight = -9999;
+    // find left max
+    int *left = mid - 1;
+    int leftSum = 0;
+    while(left - low >= 0) {
+        leftSum += *left;
+        if (leftSum > maxLeft) {
+            maxLeft = leftSum;
+            maxLow = left;
+        }
+        left--;
+    }
+    // find right max
+    int *right = mid + 1;
+    int rightSum = 0;
+    while(high - right >= 0) {
+        rightSum += *(right - 1);
+        if (rightSum > maxRight) {
+            maxRight = rightSum;
+            maxHigh = right;
+        }
+        right++;
+    }
+    low = maxLow;
+    high = maxHigh;
+    return maxLeft + maxRight;
+}
+
+int main() {
+    transformArray(P, 16);
+    int *low = P;
+    int *high = P + 15;
+    int max = findMaxSubArray(low, high);
+    return 0;
 }
--- [算法导论]第四章-分治策略-最大子数组问题/violent.cpp
+++ [算法导论]第四章-分治策略-最大子数组问题/violent.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int P[13] = {23, 13, 15, 32, 25, 27, 19, 20, 16, 12, 22, 24, 18};
+int P[15] = {23,11, 13, 5, 15, 32, 25, 27, 19, 20, 16, 12, 22, 24, 18};
+
+// 变换成差分
+void transformArray(int list[], int len) {
+    for (int i = 0; i < len - 1; i++) {
+        list[i] = list[i + 1] - list[i];
+    }
+    list[len-1] = 0;
+}
 
 void findMaxChange(int list[], int len) {
-    int max = -9999; // 可能出现负数，所以取负无穷作为初始化
+    int max = -9999;
     int maxLeft = 0, maxRight = 0;
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
@@ -16,10 +24,36 @@ void findMaxChange(int list[], int len) {
             }
         }
     }
-    cout << "Max change is " << max << ": from " << maxLeft << " to " << maxRight << endl;
+    cout << "Largest change is " << max << ": from " << maxLeft << " to " << maxRight << endl;
+}
+
+void findMaxSubArrayViolent(int list[], int len) {
+    transformArray(P, 15);
+    // 求出部分和
+    int subSumArray[14];
+    subSumArray[0] = list[0];
+    for (int i = 1; i < len; i++) {
+        subSumArray[i] = subSumArray[i - 1] + list[i];
+    }
+    // 求出最大子数组
+    int max = list[0];
+    int maxLeft = 0, maxRight = 1;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            int sum = subSumArray[j] - subSumArray[i];
+            if (sum > max) {
+                max = sum;
+                maxLeft = i;
+                maxRight = j;
+            }
+        }
+    }
+    cout << "Largest change is " << max << ": from " << maxLeft << " to " << maxRight << endl;
 }
 
 int main() {
-    findMaxChange(P, 13);
+    // 
+    findMaxSubArrayViolent(P, 13);
+    
     return 0;
 }
